6ae8037a80a786ae449eaca823e705e4c2475d13
mrmayankmathur <mrmayankmathur@gmail.com>
2024-08-19 12:41:40 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="clzx4knlb058pz2hxncvc3qsg"
+    config={{
+      appearance: {
+        theme: "light",
+      },
+      embeddedWallets: {
+        createOnLogin: "users-without-wallets",
+      },
+    }}
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </PrivyProvider>
+);
